395f69b9b3bf134eabb93a3d1d9f63d4a301295e
TheMattBin <matthewlau0101@gmail.com>
2024-10-20 22:46:04 +0800

    Added front end index

--- traditional-search/search.py
+++ traditional-search/search.py
@@ -1,16 +1,23 @@
 from flask import Flask, request, render_template
 import json
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='images')
 
-# Load mock database
-with open('mock_database.json') as f:
-    mock_db = json.load(f)
+# Load mock database from JSON file
+def load_data():
+    with open('catdogDB.json') as f:
+        return json.load(f)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/search', methods=['GET'])
 def search():
-    query = request.args.get('q').lower()
-    results = [img for img in mock_db['images'] if query in img['description'].lower()]
+    query = request.args.get('q', '').lower()
+    data = load_data()
+    results = [img for img in data['images'] if query in img['description'].lower()]
+    print(results)
     return render_template('results.html', results=results)
 
 if __name__ == '__main__':
